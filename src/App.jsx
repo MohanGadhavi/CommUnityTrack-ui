abd14c5faa4b7ee67525a788d20abf2daccedf3f
MohanGadhavi <gadhavimohan71@gmail.com>
2024-08-25 16:22:34 +0530

    update login

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { Button } from "@material-tailwind/react"
-
+import { Button } from "@material-tailwind/react";
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
 
 function App() {
-
   return (
     <>
-   <h1 className="text-5xl font-semibold">Hello</h1>
+      <div className="w-full py-8 px-5 bg-black text-white h-14 grid content-center ">
+        <h1 className="text-4xl font-bold">
+          Comm<span className="text-blue-600">Unity</span>Task
+        </h1>
+      </div>
+      <Login />
+      <Registration />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Registration/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Registration() {
+  return (
+    <div className="hover:bg-blue-800 duration-700 hover:text-white text-4xl font-bold uppercase text-blue-800">
+      <h1 className="">Registration</h1>
+    </div>
+  );
+}
+
+export default Registration;
