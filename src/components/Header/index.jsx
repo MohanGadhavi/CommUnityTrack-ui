bd47c3d8990d5e10c790534bad12b1dafb709a39
[200~AkshayBhavsar5git config --global user.name AkshayBhavsar5git config --global user.name [200~AkshayBhavsar5 <bhavsarakshay786@gmail.com>
2024-12-21 03:46:02 +0530

    userprofile file created for view,edit and logout profile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -36,6 +36,7 @@ import {} from "@fortawesome/free-regular-svg-icons";
 import { useDispatch, useSelector } from "react-redux";
 import api from "../../utils/api";
 import { logout } from "../../../store/auth";
+import UserProfile from "../../pages/UserProfile/UserProfile";
 
 // profile menu component
 const profileMenuItems = [
@@ -108,133 +109,133 @@ const eventNameList = [
   "Hackathon",
 ];
 
-function UserProfile({ userName, ppUrl }) {
-  const [openProfile, setOpenProfile] = useState(false);
-  const [confirmLogout, setConfirmLogout] = useState(false);
-  const [loading, setLoading] = useState(false);
+// function UserProfile({ userName, ppUrl }) {
+//   const [openProfile, setOpenProfile] = useState(false);
+//   const [confirmLogout, setConfirmLogout] = useState(false);
+//   const [loading, setLoading] = useState(false);
 
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+//   const navigate = useNavigate();
+//   const dispatch = useDispatch();
 
-  const handleLogout = async () => {
-    try {
-      setLoading(true);
-      await api
-        .get("/user/logout")
-        .then((res) => {
-          console.log("LogOutresssssssssssss ", res);
-        })
-        .catch(() => {
-          // localStorage.removeItem("authToken"); // Remove invalid token
-        });
-      dispatch(logout());
-      localStorage.removeItem("authToken");
-      navigate("/"); // Redirect to login
-      setConfirmLogout(!confirmLogout);
-    } catch (error) {
-      console.error("Logout error: ", error);
-      // localStorage.removeItem("authToken"); // Remove corrupted token
-    } finally {
-      setLoading(false); // Stop loading
-    }
-  };
+//   const handleLogout = async () => {
+//     try {
+//       setLoading(true);
+//       await api
+//         .get("/user/logout")
+//         .then((res) => {
+//           console.log("LogOutresssssssssssss ", res);
+//         })
+//         .catch(() => {
+//           // localStorage.removeItem("authToken"); // Remove invalid token
+//         });
+//       dispatch(logout());
+//       localStorage.removeItem("authToken");
+//       navigate("/"); // Redirect to login
+//       setConfirmLogout(!confirmLogout);
+//     } catch (error) {
+//       console.error("Logout error: ", error);
+//       // localStorage.removeItem("authToken"); // Remove corrupted token
+//     } finally {
+//       setLoading(false); // Stop loading
+//     }
+//   };
 
-  return (
-    <>
-      <Accordion
-        open={openProfile}
-        className={`  border border-gray-400 rounded-lg shadow-lg ${
-          openProfile ? "bg-purple-50 " : "bg-purple-50"
-        } `}
-        icon={
-          <FontAwesomeIcon
-            icon={faAngleDown}
-            className={` font-thin text-sm ${
-              openProfile ? "  " : " rotate-180 "
-            }`}
-          />
-        }
-        // onBlur={}
-      >
-        <AccordionHeader
-          onClick={() => setOpenProfile(!openProfile)}
-          className="p-2 px-3 border-none text-base w-full"
-        >
-          <div className="flex items-center gap-2">
-            <Avatar
-              className="w-8 h-8 outline outline-2 "
-              src={ppUrl}
-              alt="avatar"
-            />
-            <p className="text-lg ">{userName}</p>
-          </div>
-        </AccordionHeader>
-        <AccordionBody className="mt-2 p-0 pt-1 border-t border-gray-400 ">
-          <List className="px-1">
-            <ListItem className=" hover:bg-gray-300 active:bg-purple-100 focus:bg-transparent  focus:outline-gray-800 ">
-              <ListItemPrefix>
-                <FontAwesomeIcon icon={faUser} className="font-thin text-sm" />
-              </ListItemPrefix>
-              My Profile
-            </ListItem>
-            <ListItem className=" hover:bg-gray-300 active:bg-purple-100 focus:bg-transparent  focus:outline-gray-800 ">
-              <ListItemPrefix>
-                <FontAwesomeIcon icon={faGear} className="font-thin text-sm" />
-              </ListItemPrefix>
-              Edit Profile
-            </ListItem>
-            <hr className="bg-gray-400 h-0.5" />
-            <ListItem
-              className=" hover:bg-red-100 focus:bg-transparent focus:outline-red-800  text-red-500 focus:text-red-500 "
-              onClick={() => setConfirmLogout(!confirmLogout)}
-            >
-              <ListItemPrefix>
-                <FontAwesomeIcon
-                  icon={faPowerOff}
-                  className="font-thin text-sm"
-                />
-              </ListItemPrefix>
-              Log Out
-            </ListItem>
-          </List>
-        </AccordionBody>
-      </Accordion>
-      <Dialog
-        className="bg-red-50 rounded-2xl"
-        open={confirmLogout}
-        handler={() => setConfirmLogout(!confirmLogout)}
-      >
-        {/* <DialogHeader>Its a simple dialog.</DialogHeader> */}
-        <DialogBody className="">
-          <div className=" w-1/2 mx-auto flex flex-col items-center">
-            <FontAwesomeIcon
-              icon={faCircleExclamation}
-              className=" text-5xl text-red-400"
-            />
-            <p className="text-3xl text-red-400 mt-3">Are you sure?</p>
-            <p>You will be returned to the login screen.</p>
-            <div className="w-full mt-5 flex justify-between gap-4 ">
-              <Button
-                className=" w-full text-sm normal-case bg-gray-200 text-gray-800 outline outline-1 outline-gray-600 active:outline-2 focus:outline-2 active:outline-gray-800 focus:outline-gray-800"
-                onClick={() => setConfirmLogout(!confirmLogout)}
-                disabled={loading}
-              >
-                Cencel
-              </Button>
-              <Button
-                className=" w-full text-sm normal-case bg-red-600 justify-center"
-                onClick={handleLogout}
-                loading={loading}
-              >
-                Logout
-              </Button>
-            </div>
-          </div>
-        </DialogBody>
-      </Dialog>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <Accordion
+//         open={openProfile}
+//         className={`  border border-gray-400 rounded-lg shadow-lg ${
+//           openProfile ? "bg-purple-50 " : "bg-purple-50"
+//         } `}
+//         icon={
+//           <FontAwesomeIcon
+//             icon={faAngleDown}
+//             className={` font-thin text-sm ${
+//               openProfile ? "  " : " rotate-180 "
+//             }`}
+//           />
+//         }
+//         // onBlur={}
+//       >
+//         <AccordionHeader
+//           onClick={() => setOpenProfile(!openProfile)}
+//           className="p-2 px-3 border-none text-base w-full"
+//         >
+//           <div className="flex items-center gap-2">
+//             <Avatar
+//               className="w-8 h-8 outline outline-2 "
+//               src={ppUrl}
+//               alt="avatar"
+//             />
+//             <p className="text-lg ">{userName}</p>
+//           </div>
+//         </AccordionHeader>
+//         <AccordionBody className="mt-2 p-0 pt-1 border-t border-gray-400 ">
+//           <List className="px-1">
+//             <ListItem className=" hover:bg-gray-300 active:bg-purple-100 focus:bg-transparent  focus:outline-gray-800 ">
+//               <ListItemPrefix>
+//                 <FontAwesomeIcon icon={faUser} className="font-thin text-sm" />
+//               </ListItemPrefix>
+//               My Profile
+//             </ListItem>
+//             <ListItem className=" hover:bg-gray-300 active:bg-purple-100 focus:bg-transparent  focus:outline-gray-800 ">
+//               <ListItemPrefix>
+//                 <FontAwesomeIcon icon={faGear} className="font-thin text-sm" />
+//               </ListItemPrefix>
+//               Edit Profile
+//             </ListItem>
+//             <hr className="bg-gray-400 h-0.5" />
+//             <ListItem
+//               className=" hover:bg-red-100 focus:bg-transparent focus:outline-red-800  text-red-500 focus:text-red-500 "
+//               onClick={() => setConfirmLogout(!confirmLogout)}
+//             >
+//               <ListItemPrefix>
+//                 <FontAwesomeIcon
+//                   icon={faPowerOff}
+//                   className="font-thin text-sm"
+//                 />
+//               </ListItemPrefix>
+//               Log Out
+//             </ListItem>
+//           </List>
+//         </AccordionBody>
+//       </Accordion>
+//       <Dialog
+//         className="bg-red-50 rounded-2xl"
+//         open={confirmLogout}
+//         handler={() => setConfirmLogout(!confirmLogout)}
+//       >
+//         {/* <DialogHeader>Its a simple dialog.</DialogHeader> */}
+//         <DialogBody className="">
+//           <div className=" w-1/2 mx-auto flex flex-col items-center">
+//             <FontAwesomeIcon
+//               icon={faCircleExclamation}
+//               className=" text-5xl text-red-400"
+//             />
+//             <p className="text-3xl text-red-400 mt-3">Are you sure?</p>
+//             <p>You will be returned to the login screen.</p>
+//             <div className="w-full mt-5 flex justify-between gap-4 ">
+//               <Button
+//                 className=" w-full text-sm normal-case bg-gray-200 text-gray-800 outline outline-1 outline-gray-600 active:outline-2 focus:outline-2 active:outline-gray-800 focus:outline-gray-800"
+//                 onClick={() => setConfirmLogout(!confirmLogout)}
+//                 disabled={loading}
+//               >
+//                 Cencel
+//               </Button>
+//               <Button
+//                 className=" w-full text-sm normal-case bg-red-600 justify-center"
+//                 onClick={handleLogout}
+//                 loading={loading}
+//               >
+//                 Logout
+//               </Button>
+//             </div>
+//           </div>
+//         </DialogBody>
+//       </Dialog>
+//     </>
+//   );
+// }
 
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
@@ -302,7 +303,6 @@ function Header() {
           ))}
         </ul>
       </div>
-
       {/* Profile  */}
       <div className=" w-full  p-2 absolute bottom-0">
         <UserProfile
